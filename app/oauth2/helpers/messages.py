23fd57384f5bc81c42d20e5d7c81eaeda22445f3
ali <alihezarpisheh@outlook.com>
2026-08-08 14:08:53 +0330

    💬 Chore(oauth2): Introduce an enumeration class for response messages

---
+++ app/oauth2/helpers/messages.py
@@ -0,0 +1,21 @@
+"""Module holding enumerations related to response messages."""
+
+from enum import StrEnum
+
+
+class ClientMessages(StrEnum):
+    """Enumeration of client-related messages, used in API responses."""
+
+    SUCCESSFUL_CLIENT_REGISTRATION = "Client registered successfully."
+
+
+class ScopeMessages(StrEnum):
+    """Enumeration of scope-related messages, used in API responses."""
+
+    SUCCESSFUL_SCOPE_CREATION = "Scope created successfully."
+    SUCCESSFUL_SCOPE_MODIFICATION = "Scope modified successfully."
+    SCOPE_ALREADY_EXIST = "Scope {scope} already exist in the system."
+    SCOPE_DOES_NOT_EXISTS = "Scope doesn't exist in the system."
+    SCOPE_CAN_NOT_BE_DELETED = (
+        "Selected scope can not be deleted, because it is assigned to a client."
+    )
