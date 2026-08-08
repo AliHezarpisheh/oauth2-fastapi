fd2f0133bf0e7f5992f9fac553a901b923855d02
ali <alihezarpisheh@outlook.com>
2026-08-08 14:01:27 +0330

    🚨 Refactor(toolkit): Fix `ruff` issues

--- toolkit/api/exceptions/__init__.py
+++ toolkit/api/exceptions/__init__.py
@@ -1,5 +1,6 @@
 from .abc import APIException
 from .custom_exceptions import (
+    BadRequestError,
     DoesNotExistError,
     DuplicateError,
     InternalServerError,
@@ -9,6 +10,7 @@ from .custom_exceptions import (
 
 __all__ = [
     "APIException",
+    "BadRequestError",
     "DoesNotExistError",
     "DuplicateError",
     "InternalServerError",
