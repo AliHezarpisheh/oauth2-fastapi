568c0455fd88daafa97bba3079b7303bef052b55
ali <alihezarpisheh@outlook.com>
2026-08-20 14:49:14 +0330

    ✨ Feature(toolkit): Enhance `http_headers` attribute on `APIException`
    Add `default_http_headers` class attribute, enabling the class to define
    http headers while getting defined, also turn `http_headers` to a
    property which extends the class headers with instance headers.

--- toolkit/api/exceptions/abc.py
+++ toolkit/api/exceptions/abc.py
@@ -1,12 +1,15 @@
 """Module holding abstract base classes for the application."""
 
 from abc import ABC, abstractmethod
+from typing import ClassVar
 
 from toolkit.api.enums import HTTPStatusDoc, Status
 
 
 class APIException(ABC, Exception):
     """Base exception for all API-related errors, handled in exception handlers."""
+
+    default_http_headers: ClassVar[dict[str, str] | None] = None
 
     def __init__(
         self,
@@ -19,16 +22,16 @@ class APIException(ABC, Exception):
         Initialize an `APIException` object.
 
         Both of the `field` and `reason` should be set, or both should be leaved unset.
-        If one of them is set and the other is unset, an `AssertionError` will be
+        If one of them is set and the other is unset, an `ValueError` will be
         raised.
         """
         if bool(field) ^ bool(reason):
-            raise ValueError("`field` and `reason` should be both set or unset")
+            raise ValueError("`field` and `reason` should be both set or unset.")
 
         self.message = message
         self.field = field
         self.reason = reason
-        self.http_headers = http_headers
+        self._http_headers = http_headers
         super().__init__(message)
 
     @property
@@ -45,6 +48,26 @@ class APIException(ABC, Exception):
     @abstractmethod
     def documentation_link(self) -> HTTPStatusDoc:
         """URL to relevant documentation."""
+
+    @property
+    def http_headers(self) -> dict[str, str] | None:
+        """
+        Return the effective HTTP headers for the response.
+
+        Notes
+        -----
+        If you set same headers/keys on both instance and class levels, the instance
+        level will be included in the final return value.
+        """
+        if self._http_headers is None and self.default_http_headers is None:
+            return None
+        if self._http_headers is not None and self.default_http_headers is not None:
+            return {**self.default_http_headers, **self._http_headers}
+        return (
+            self._http_headers
+            if self._http_headers is not None
+            else self.default_http_headers
+        )
 
     def to_jsonable_dict(self) -> dict[str, str | dict[str, str]]:
         """Return error data as a jsonable dictionary."""
