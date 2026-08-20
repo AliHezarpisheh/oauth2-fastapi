ffdbb959a4ac680f086cfd69585bad1e41992a53
ali <alihezarpisheh@outlook.com>
2026-08-20 14:55:58 +0330

    ✨ Feature(oauth2): Add initial access token check at client registration

--- app/oauth2/api/routers/client.py
+++ app/oauth2/api/routers/client.py
@@ -2,7 +2,7 @@
 
 from typing import Annotated, Any
 
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, BackgroundTasks, Depends, Header, status
 
 from app.oauth2.api.dependencies import get_client_service
 from app.oauth2.repository.services import ClientService
@@ -22,10 +22,16 @@ router = APIRouter(prefix="/register", tags=[OpenAPITags.OAUTH2])
     status_code=status.HTTP_201_CREATED,
 )
 async def register_client(
+    bearer_token: Annotated[str, Header(alias="Authorization")],
     client_registration_input: ClientRegistrationRequestSchema,
+    background_tasks: BackgroundTasks,
     client_service: Annotated[ClientService, Depends(get_client_service)],
 ) -> dict[str, Any]:
     """Register a new OAuth2 client application."""
+    await client_service.check_initial_access_token(
+        bearer_token=bearer_token,
+        background_tasks=background_tasks,
+    )
     return await client_service.register_client(
         client_registration_input=client_registration_input
     )
